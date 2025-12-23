868c1bd6f2fc7d0b942cafa7fbf0081b398a89bc
Naeem-Shuvo <2305146@ugrad.cse.buet.ac.bd>
2025-12-23 21:06:28 +0600

    1st class, learnt live server, html and css , how to edit elements of html and how to style using css

---
+++ 1st vid/index.js
@@ -0,0 +1,8 @@
+console.log(`Hello World!`);
+
+//create alert box
+
+// window.alert("This is an alert box!");
+// window.alert("Welcome to JS world");
+
+document.getElementById("heading").textContent = "Heading changed using JS";
