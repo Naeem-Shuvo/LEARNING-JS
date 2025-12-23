1fe0c44815f6c6f4e374832320fd4eb3f146e674
Naeem-Shuvo <2305146@ugrad.cse.buet.ac.bd>
2025-12-24 00:06:38 +0600

    Add class 3 folder

---
+++ class 3/index.js
@@ -0,0 +1,12 @@
+//variables
+ let userName="john";
+    let age=25;
+    let cg=3.8;
+    let isStudent=true;
+    let hobbies=["reading","gaming","coding"];
+    let address={
+        street:"123 Main St",
+        city:"New York",
+        zip:"10001"
+    };
+    document.getElementById("p1").textContent=`Name: ${userName}`;
