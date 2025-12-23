5d242962eb1a977540505897cbee8ff0c78ac41f
Naeem-Shuvo <2305146@ugrad.cse.buet.ac.bd>
2025-12-23 23:27:07 +0600

    how to show variable's data on HTML

--- 2/index.js
+++ 2/index.js
@@ -9,20 +9,4 @@
         city:"New York",
         zip:"10001"
     };
-    console.log(`your name is ${userName} and age is ${age}`);
-    console.log(`your cg is ${cg} and its not good`);
-
-    console.log(typeof isStudent);
-    console.log(typeof address);
-    console.log(typeof hobbies); //object as well
-    
-    // Proper ways to check for arrays:
-    console.log(Array.isArray(hobbies)); // true
-    console.log(hobbies instanceof Array); // true
-    console.log(Object.prototype.toString.call(hobbies)); // "[object Array]"
-
-    console.log(userName);
-    console.log(age);
-    console.log(isStudent);
-    console.log(hobbies);
-    console.log(address);
+    document.getElementById("p1").textContent=`Name: ${userName}`;
