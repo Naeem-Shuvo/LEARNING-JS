5f6d4d7deb31feb19839a513cb022fa77a08fb5c
Naeem-Shuvo <2305146@ugrad.cse.buet.ac.bd>
2025-12-26 00:53:19 +0600

    string methods elaborated with INSTRUCTIONS as COMMENTS

---
+++ strings method/strings.js
@@ -0,0 +1,34 @@
+// string methods = allow you to manipulate and work with text (strings)
+
+let userName = "Bro Code";
+let phoneNumber = "123-456-7890";
+
+// console.log(userName.length);
+// console.log(userName.charAt(userName.length - 1));
+// console.log(userName.indexOf("o"));
+// console.log(userName.lastIndexOf("o"));
+// userName = userName.trim(); //removes whitespace from both ends of a string,not within the string
+// console.log(userName);  // Add this to see the result
+// userName = userName.toUpperCase();
+//userName = userName.toLowerCase();
+
+//**HOW MAY TIMES TO REPEAT A STRING
+// userName = userName.repeat(3);
+
+//**TO CHECK IF A STRING STARTS WITH A SPECIFIC CHARACTER OR SUBSTRING
+// let result = userName.startsWith("Br");
+// console.log(result);
+
+//**TO CHECK IF IT CONTAINS A SPECIFIC CHARACTER OR SUBSTRING
+//let result = userName.endsWith(" ");
+// let result = userName.includes(" ");
+// console.log(result);
+
+// phoneNumber = phoneNumber.replaceAll("-", "");
+
+//**PADS WITH 0 TO MAKE IT A 15 CHARACTER STRING */
+// phoneNumber = phoneNumber.padStart(15, "0");
+
+//phoneNumber = phoneNumber.padEnd(15, "0");
+
+console.log(phoneNumber);
