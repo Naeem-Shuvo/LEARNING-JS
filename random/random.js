0d0d39cb1783198a0ca6b3de74b20412294f4326
Naeem-Shuvo <2305146@ugrad.cse.buet.ac.bd>
2025-12-25 13:49:14 +0600

    JS for random generator done

--- random/random.js
+++ random/random.js
@@ -1,10 +1,5 @@
-let randomNum=Math.random();
-console.log(Math.floor(randomNum*6)); //0 to 5
-console.log(Math.floor(randomNum*6)+1); //1 to 6
-console.log(Math.floor(randomNum*10)+1); //1 to 10
-console.log(Math.floor(randomNum*100)+1); //1 to 100
-
-// 51 to 100
-const min=50;
-const max=100;
-console.log(Math.floor(randomNum*(max-min))+min+1);
+document.getElementById("randBtn").onclick=function(){
+    let randomNum=Math.random(); 
+    //this should be inside ONCLICK cz each time we click we want a new random number
+    document.getElementById("randLabel").textContent=Math.floor(randomNum*100)+1;
+}
