d4d3f899c053e4a77d0af647ebdc1a99598971d9
Naeem-Shuvo <2305146@ugrad.cse.buet.ac.bd>
2025-12-28 16:55:11 +0600

    capitalized and stored in original, then callback calls Print()

--- forEach()/forEach.js
+++ forEach()/forEach.js
@@ -10,12 +10,13 @@ let fruits = ["apple", "banana", "cherry"];
 //***it will call print function over each element of the array***
 
 //**used callback, not necessary */
-fruits.forEach(item => uppercaseFruit(item, print)); // Output: APPLE, BANANA, CHERRY
+fruits.forEach((item, index, array) => capitalizeFruit(item, index, array, print)); // Output: APPLE, BANANA, CHERRY
 
-function uppercaseFruit(item, callback) {
-    const uppercased = item.toUpperCase();
-    callback(uppercased); // Use the callback to print
+function capitalizeFruit(item, index, array, callback) {
+    array[index]=item.charAt(0).toUpperCase()+item.slice(1);
+    callback(array[index]); // Use the callback to print
 }
+
 function print(item) { //only taking first parameter 
     console.log(item);
 }
